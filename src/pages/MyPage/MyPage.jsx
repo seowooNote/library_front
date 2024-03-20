3b27bde3ca1c8676bbf831054b90d7b57739cb19
seowooNote <dkr0210@naver.com>
2024-03-20 17:52:23 +0900

    이메일 인증 완료

--- src/pages/MyPage/MyPage.jsx
+++ src/pages/MyPage/MyPage.jsx
@@ -1,41 +1,68 @@
 /** @jsxImportSource @emotion/react */
-import { useQueryClient } from "react-query";
+import { useMutation, useQueryClient } from "react-query";
 import * as s from "./style";
+import { sendAuthMailRequest } from "../../apis/api/sendAuthMail";
+import FullSizeLoader from "../../components/FullSizeLoader/FullSizeLoader";
+import { GoCheckCircle } from "react-icons/go";
 
 function MyPage() {
 
   const queryClient = useQueryClient();
   const principalData = queryClient.getQueryData("principalQuery");
 
-  return (
-    <div css={s.layout}>
-        <div css={s.header}>
-          <div css={s.imgBox}>
-            <div css={s.profileImg}>
-              <img src="https://i.namu.wiki/i/10w2BNa-ArEINXB3Lh8RMB2-ETf-tqKk4zkdOa1_J-jjgwCG0mbs8D5dE6GbETQ4dBHxa82fNA1-6hYy4oum_g.gif" alt="" />
-            </div>
-          </div>
-          <div css={s.infoBox}>
-            <div css={s.infoText}>사용자 이름: {principalData.data.username}</div>
-            <div css={s.infoText}>이름: {principalData.data.name}</div>
-            <div css={s.emailBox}>
-              <div css={s.infoText}>이메일: {principalData.data.email}</div>
-              {principalData.data.authorities.filter((auth) => auth.authority === "ROLE_USER").length === 0 
-              ? <button css={s.infoButton}>인증하기</button> 
-              : <>체크</>
-              }
-              
-            </div>
-            <div>
-              <button>정보 수정</button>
-              <button>비밀번호 수정</button>
-            </div>
-          </div>
-        </div>
-        <div css={s.bottom}>
+  const sendAuthMailMutation = useMutation({
+    mutationKey: "sendAuthMailMutation",
+    mutationFn: sendAuthMailRequest,
+    retry: 0,
+    onSuccess: (response) => {
+      if(response) {
+        alert("메일 전송을 완료했습니다.");
+      }else {
+        alert("메일 전송에 실패하였습니다.");
+      }
+    }
+  });
 
-        </div>
-    </div>
+  const handleSendAuthMailClick = () => {
+    sendAuthMailMutation.mutate();
+  }
+
+  return (
+    <>
+      {
+        sendAuthMailMutation.isLoading ?
+          <FullSizeLoader/>
+        : 
+          <div css={s.layout}>
+              <div css={s.header}>
+                <div css={s.imgBox}>
+                  <div css={s.profileImg}>
+                    <img src="https://i.namu.wiki/i/10w2BNa-ArEINXB3Lh8RMB2-ETf-tqKk4zkdOa1_J-jjgwCG0mbs8D5dE6GbETQ4dBHxa82fNA1-6hYy4oum_g.gif" alt="" />
+                  </div>
+                </div>
+                <div css={s.infoBox}>
+                  <div css={s.infoText}>사용자 이름: {principalData.data.username}</div>
+                  <div css={s.infoText}>이름: {principalData.data.name}</div>
+                  <div css={s.emailBox}>
+                    <div css={s.infoText}>이메일: {principalData.data.email}</div>
+                    {
+                    principalData.data.authorities.filter((auth) => auth.authority === "ROLE_USER").length === 0 
+                    ? <button css={s.infoButton} onClick={handleSendAuthMailClick}>인증하기</button> 
+                    : <div css={s.emailCheck}><GoCheckCircle /></div>
+                    }
+                  </div>
+                  <div css={s.infoButtons}>
+                    <button css={s.infoButton}>정보 수정</button>
+                    <button css={s.infoButton}>비밀번호 수정</button>
+                  </div>
+                </div>
+              </div>
+              <div css={s.bottom}>
+
+              </div>
+          </div>
+      }
+    </>
   )
 }
 
--- src/pages/MyPage/style.js
+++ src/pages/MyPage/style.js
@@ -62,14 +62,32 @@ export const emailBox = css`
     align-items: center;
     margin-bottom: 10px;
 
-    & > div {
-        margin: 0;
+    & > div:nth-of-type(1) {
+        margin: 0px 10px 0px 0px;
+    }
+`;
+
+export const emailCheck = css`
+    display: flex;
+    align-items: center;
+    
+    & > * {
+        color: #008734;
+    }
+`;
+
+export const infoButtons = css`
+    box-sizing: border-box;
+    display: flex;
+    padding-top: 5px;
+
+    & > button:nth-of-type(1) {
+        margin-right: 10px;
     }
 `;
 
 export const infoButton = css`
     box-sizing: border-box;
-    margin-left: 10px;
     border: 1px solid #dbdbdb;
     border-radius: 3px;
     padding: 5px;
@@ -85,6 +103,8 @@ export const infoButton = css`
         background-color: #eeeeee;
     }
 `;
+
+
 
 export const bottom = css`
     box-sizing: border-box;
